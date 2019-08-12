de6c444933ddc71543343cff3e04113baabdfcc1
udithindrakantha <udith.indrakantha@gmail.com>
2019-08-12 23:49:31 +0530

    Initial commit with errors

---
+++ WebChatApp/src/main/java/com/udith/WebChatApp/WebChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.udith.WebChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatAppApplication.class, args);
+	}
+
+}
